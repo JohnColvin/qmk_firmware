d5f2f977d799c01456444476e72220d6f3b3a904
John Colvin <colvin.john@gmail.com>
2018-03-21 18:00:08 -0400

    Move thumbs around, fix delete position, remove window close keys

--- keyboards/mitosis/keymaps/2john4tv/keymap.c
+++ keyboards/mitosis/keymaps/2john4tv/keymap.c
@@ -11,11 +11,11 @@ enum mitosis_keycodes {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // LETTERS
   [0] = {
-    {_______,     KC_W,        KC_F,          KC_P,       _______,              _______,  KC_L,       KC_U,             KC_Y,         _______      },
-    {LT(6,KC_A),  LT(2,KC_R),  LT(8,KC_S),    LT(3,KC_T), KC_D,                 KC_H,     LT(7,KC_N), LT(8,KC_E),       LT(2,KC_I),   LT(4,KC_O)   },
-    {CTL_T(KC_Z), ALT_T(KC_X), GUI_T(KC_C),   KC_G,       KC_B,                 KC_K,     LT(5,KC_M), RGUI_T(KC_J),     ALGR_T(KC_Q), RCTL_T(KC_V) },
-    {XXXXXXX,     _______,     _______,       _______,    _______,              _______,  _______,    _______,          _______,      XXXXXXX      },
-    {XXXXXXX,     _______,     SFT_T(KC_TAB), TT(1),      KC_BSPACE,            KC_SPACE, KC_ENTER,   SFT_T(KC_ESCAPE), _______,      XXXXXXX      }
+    {_______,     KC_W,        KC_F,          KC_P,       _______,              _______,          KC_L,       KC_U,         KC_Y,         _______      },
+    {LT(6,KC_A),  LT(2,KC_R),  LT(8,KC_S),    LT(3,KC_T), KC_D,                 KC_H,             LT(7,KC_N), LT(8,KC_E),   LT(2,KC_I),   LT(4,KC_O)   },
+    {CTL_T(KC_Z), ALT_T(KC_X), GUI_T(KC_C),   KC_G,       KC_B,                 KC_K,             LT(5,KC_M), RGUI_T(KC_J), ALGR_T(KC_Q), RCTL_T(KC_V) },
+    {XXXXXXX,     _______,     _______,       TT(1),      KC_BSPACE,            KC_SPACE,         KC_ENTER,   _______,      _______,      XXXXXXX      },
+    {XXXXXXX,     _______,     _______,       _______,    SFT_T(KC_TAB),        SFT_T(KC_ESCAPE),  _______,   _______,      _______,      XXXXXXX      }
   },
 
   // MOUSE & MEDIA
@@ -47,11 +47,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // COPY/PASTE
   [4] = {
-    {_______,    _______,    _______,    _______,    _______,      LGUI(KC_Q), _______, _______, _______, LGUI(KC_W) },
-    {LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), _______,      _______,    _______, _______, _______, _______    },
-    {_______,    _______,    _______,    _______,    _______,      _______,    _______, _______, _______, _______    },
-    {XXXXXXX,    _______,    _______,    _______,    _______,      _______,    _______, _______, _______, XXXXXXX    },
-    {XXXXXXX,    _______,    _______,    _______,    KC_DELETE,    _______,    _______, _______, _______, XXXXXXX    }
+    {_______,    _______,    _______,    _______,    _______,      _______, _______, _______, _______, _______ },
+    {LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), _______,      _______, _______, _______, _______, _______ },
+    {_______,    _______,    _______,    _______,    _______,      _______, _______, _______, _______, _______ },
+    {XXXXXXX,    _______,    _______,    _______,    KC_DELETE,    _______, _______, _______, _______, XXXXXXX },
+    {XXXXXXX,    _______,    _______,    _______,    _______,      _______, _______, _______, _______, XXXXXXX }
   },
 
   // NUMBERS
